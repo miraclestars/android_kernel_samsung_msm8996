cc2dee103e685c24cd456322c1066f57eba34cf1
Morten Rasmussen <morten.rasmussen@arm.com>
2014-12-18 14:47:18 +0000

    sched: Calculate energy consumption of sched_group
    For energy-aware load-balancing decisions it is necessary to know the
    energy consumption estimates of groups of cpus. This patch introduces a
    basic function, sched_group_energy(), which estimates the energy
    consumption of the cpus in the group and any resources shared by the
    members of the group.
    
    NOTE: The function has five levels of identation and breaks the 80
    character limit. Refactoring is necessary.
    
    Change-Id: Ia93c66bb272f082044c7b26f5d716cb99fe5bed5
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9404,6 +9404,7 @@ DEFINE_PER_CPU(struct sched_domain *, sd_numa);
 DEFINE_PER_CPU(struct sched_domain *, sd_busy);
 DEFINE_PER_CPU(struct sched_domain *, sd_asym);
 DEFINE_PER_CPU(struct sched_domain *, sd_ea);
+DEFINE_PER_CPU(struct sched_domain *, sd_scs);
 
 static void update_top_cache_domain(int cpu)
 {
@@ -9437,6 +9438,9 @@ static void update_top_cache_domain(int cpu)
 			break;
 	}
 	rcu_assign_pointer(per_cpu(sd_ea, cpu), ea_sd);
+
+	sd = highest_flag_domain(cpu, SD_SHARE_CAP_STATES);
+	rcu_assign_pointer(per_cpu(sd_scs, cpu), sd);
 }
 
 /*
