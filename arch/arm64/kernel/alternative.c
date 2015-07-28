1ae0a4dbd6a545c61061960bc9dc56e923c636df
Will Deacon <will.deacon@arm.com>
2015-07-28 19:07:28 +0100

    UPSTREAM: arm64: alternative: put secondary CPUs into polling loop during patch
    When patching the kernel text with alternatives, we may end up patching
    parts of the stop_machine state machine (e.g. atomic_dec_and_test in
    ack_state) and consequently corrupt the instruction stream of any
    secondary CPUs.
    
    This patch passes the cpu_online_mask to stop_machine, forcing all of
    the CPUs into our own callback which can place the secondary cores into
    a dumb (but safe!) polling loop whilst the patching is carried out.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Bug: 31432001
    Change-Id: Iaa1aa9f7f1ac95221ef3d076685d7ca32e5b3b32
    (cherry picked from commit ef5e724b25c9f90b7683bb2d45833ebac0989dcb)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/kernel/alternative.c
+++ arch/arm64/kernel/alternative.c
@@ -42,7 +42,7 @@ struct alt_region {
  * Check if the target PC is within an alternative block.
  */
 
-static int __apply_alternatives(void *alt_region)
+static void __apply_alternatives(void *alt_region)
 {
 	struct alt_instr *alt;
 	u32 *origptr, *replptr, *endptr;
@@ -63,19 +63,38 @@ static int __apply_alternatives(void *alt_region)
 		for (; origptr < endptr; origptr++, replptr++)
 			BUG_ON(aarch64_insn_patch_text_nosync(origptr, *replptr));
 	}
-
-	return 0;
 }
 
-void apply_alternatives_all(void)
+/*
+ * We might be patching the stop_machine state machine, so implement a
+ * really simple polling protocol here.
+ */
+static int __apply_alternatives_multi_stop(void *unused)
 {
+	static int patched = 0;
 	struct alt_region region = {
 		.begin	= __alt_instructions,
 		.end	= __alt_instructions_end,
 	};
 
+	/* We always have a CPU 0 at this point (__init) */
+	if (smp_processor_id()) {
+		while (!READ_ONCE(patched))
+			cpu_relax();
+	} else {
+		BUG_ON(patched);
+		__apply_alternatives(&region);
+		/* Barriers provided by the cache flushing */
+		WRITE_ONCE(patched, 1);
+	}
+
+	return 0;
+}
+
+void __init apply_alternatives_all(void)
+{
 	/* better not try code patching on a live SMP system */
-	stop_machine(__apply_alternatives, &region, NULL);
+	stop_machine(__apply_alternatives_multi_stop, NULL, cpu_online_mask);
 }
 
 void apply_alternatives(void *start, size_t length)
