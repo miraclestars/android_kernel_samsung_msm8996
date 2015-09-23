96c0058b0b2ad73ce1c0731d7ed99df2d88124ba
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-23 12:47:48 +0100

    arm: Enable frequency invariant scheduler load-tracking support
    Defines arch_scale_freq_capacity() to use cpufreq implementation.
    
    Change-Id: I7b82e9935533aaf2f24903bb05e7f0b1bcdb6a88
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -3,6 +3,7 @@
 
 #ifdef CONFIG_ARM_CPU_TOPOLOGY
 
+#include <linux/cpufreq.h>
 #include <linux/cpumask.h>
 
 struct cputopo_arm {
@@ -23,6 +24,10 @@ extern struct cputopo_arm cpu_topology[NR_CPUS];
 void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
+
+#ifdef CONFIG_CPU_FREQ
+#define arch_scale_freq_capacity cpufreq_scale_freq_capacity
+#endif
 
 #else
 
