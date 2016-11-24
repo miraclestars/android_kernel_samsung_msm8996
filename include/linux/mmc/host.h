2ecd23c9c50aa49f9d22b3cb2e3bd8c1cb3ad8d8
Sayali Lokhande <sayalil@codeaurora.org>
2016-11-24 18:08:26 +0530

    mmc: host: Add Ring buffer logging for MMC.
    Add ring buffer to enable tracing of significant events
    of eMMC in both CQ and Legacy mode for debug purpose.
    
    CONFIG_MMC_SDHCI_RING_BUFFER is added to enable ring
    buffer logging.
    
    Change-Id: Iceb404d77fca19e133c319bc66ec15b2d77c7c54
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -21,6 +21,7 @@
 #include <linux/mmc/core.h>
 #include <linux/mmc/card.h>
 #include <linux/mmc/pm.h>
+#include <linux/mmc/ring_buffer.h>
 
 #define MMC_AUTOSUSPEND_DELAY_MS	3000
 
@@ -565,6 +566,7 @@ struct mmc_host {
 	} perf;
 	bool perf_enable;
 #endif
+	struct mmc_trace_buffer trace_buf;
 	enum dev_state dev_status;
 	bool			wakeup_on_idle;
 	struct mmc_cmdq_context_info	cmdq_ctx;
