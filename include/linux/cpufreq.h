c5cd777407e3335aa95206651996fdc9363e8be5
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:57:13 +1100

    cpufreq: Darkness Governor
    It's based on nightmare but more simple and fast, basic configs but very complex structure. It is an updated version of the nightmare gov, so far it is quite stable in tests.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -630,6 +630,9 @@ extern struct cpufreq_governor cpufreq_gov_cyan;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
