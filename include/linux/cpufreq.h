d0756182cec79662d6639e097ba6b2b13068dc41
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:57:18 +1100

    cpufreq: Electrodemand Governor
    Based on the ondemand cpu governor, this is the older governor that was used in the electroactive kernel which uses the same tunables found in the original ondemand governor.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -642,6 +642,9 @@ extern struct cpufreq_governor cpufreq_gov_despair;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DYNAMIC_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_dynamic_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dynamic_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRODEMAND)
+extern struct cpufreq_governor cpufreq_gov_electrodemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electrodemand)
 #endif
 
 /*********************************************************************
