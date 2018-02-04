3163eab294cab6882043e902f2c19545934a5b4a
djb77 <dwayne.bakewell@gmail.com>
2018-02-05 00:58:54 +1100

    cpufreq: Wheatley Governor
    This governor is build on “ondemand” but increases the C4 (the sleep state) state time of the CPU and doing so trying to save juice. So the results show that Wheatley works as intended and ensures that the C4 state is used whenever the task allows a proper efficient usage of the C4 state. For more demanding tasks which cause a large number of wakeups and prevent the efficient usage of the C4 state, the governor resorts to the next best power saving mechanism and scales down the frequency. So with the new highly-flexible Wheatley governor one can have the best of both worlds. Obviously, this governor is only available on multi-core devices.
    
    Wheatley is a more performance orientated governor as it scales more aggressively than ondemand and sticks with higher frequencies.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -747,6 +747,9 @@ extern struct cpufreq_governor cpufreq_gov_uberdemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WAVE)
 extern struct cpufreq_governor cpufreq_gov_wave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wave)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
 #endif
 
 /*********************************************************************
