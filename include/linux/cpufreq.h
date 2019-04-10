d8a67b7e1f1bcd0b75187394abd86a251bcab810
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-04-10 23:09:06 +0900

    cpufreq: OndemandX Governor
    Basically an ondemand with suspend/wake profiles. No further optimization was done to Ondemand to keep it close to source as possible.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -702,6 +702,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND_X)
+extern struct cpufreq_governor cpufreq_gov_ondemand_x;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand_x)
 #endif
 
 /*********************************************************************
