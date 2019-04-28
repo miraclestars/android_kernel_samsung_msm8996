3bae82c9569377fca0afe68bd51d9f1fa42bf4dd
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-04-29 00:56:54 +0900

    cpufreq: Clarity Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -615,6 +615,9 @@ extern struct cpufreq_governor cpufreq_gov_cafactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
 extern struct cpufreq_governor cpufreq_gov_chill;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CLARITY)
+extern struct cpufreq_governor cpufreq_gov_clarity;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_clarity)
 #endif
 
 /*********************************************************************
