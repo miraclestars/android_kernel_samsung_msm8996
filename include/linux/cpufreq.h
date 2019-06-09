4e57215b64b2b00a0c571351e190bf28b2c0c4bf
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-09 22:07:57 +0900

    cpufreq: Abyssplug Governor
    It's a Governor derived from hotplug, it works the same way, but with the changes in savings for more battery life.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -558,6 +558,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
+extern struct cpufreq_governor cpufreq_gov_abyssplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_abyssplug)
 #endif
 
 /*********************************************************************
