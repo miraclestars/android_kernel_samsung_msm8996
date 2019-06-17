2166e997caac967343c2dfc54e2935517ff65993
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-17 19:17:52 +0900

    cpufreq: Boostactive Governor
    Based on Interactive but with cpu frequency boosting capabilities. This is performance oriented governor.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -599,6 +599,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BOOSTACTIVE)
+extern struct cpufreq_governor cpufreq_gov_boostactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_boostactive)
 #endif
 
 /*********************************************************************
