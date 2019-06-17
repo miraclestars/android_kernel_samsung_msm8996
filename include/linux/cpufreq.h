230bfb9be3576046d9f6775fdc642148fa903d26
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-17 22:27:40 +0900

    cpufreq: Dynamic Interactive Governor
    This governor dynamically adjusts itself according to load. That means it's settings are dynamic (always changing) and not static (not changing). Dyninteractive still obtains the same great balance between battery life and performance found in the original interactive governor and improves it even further. This is not the same as the original interactive governor because of this unique behavior.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -639,6 +639,9 @@ extern struct cpufreq_governor cpufreq_gov_darknessv5;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
 extern struct cpufreq_governor cpufreq_gov_despair;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_despair)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DYNAMIC_INTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_dynamic_interactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dynamic_interactive)
 #endif
 
 /*********************************************************************
