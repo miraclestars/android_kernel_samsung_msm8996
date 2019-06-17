7b15c366394a8fe3f78f0423723959da5c176e86
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-17 11:43:55 +0900

    cpufreq: BadAss Governor
    Badass removes all of this "fast peaking" to the max frequency. To trigger a frequency increase, the system must run a bit with high load, then the frequency is bumped. If that is still not enough the governor gives you full throttle. (this transition should not take longer than 1-2 seconds, depending on the load your system is experiencing)
    Badass will also take the gpu load into consideration. If the gpu is moderately busy it will bypass the above check and clock the cpu to max frequency. If the gpu is crushed under load, badass will lift the restrictions to the cpu.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -587,6 +587,9 @@ extern struct cpufreq_governor cpufreq_gov_arteractive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ASSWAX)
 extern struct cpufreq_governor cpufreq_gov_asswax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_asswax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_badass)
 #endif
 
 /*********************************************************************
