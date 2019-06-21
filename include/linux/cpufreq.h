65a04123d8d876a1908a1a97dda66968dfcf844b
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-21 23:52:28 +0900

    cpufreq: Thunderstorms Interactive Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -735,6 +735,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_THUNDERSTORMS_INTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_thunderstorms_interactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_thunderstorms_interactive)
 #endif
 
 /*********************************************************************
