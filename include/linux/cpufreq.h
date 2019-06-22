7b066e4422b58dab61033ed917b01456446b585e
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-06-23 02:57:38 +0900

    cpufreq: Dance Dance Governor
    Based on conservative with some smartass features, it scales accordingly to conservatives laws. So it will start from the bottom, take a load sample, if it's above the upthreshold, ramp up only one speed at a time, and ramp down one at a time. It will automatically cap the off screen speeds to 245Mhz, and if your min freq is higher than 245mhz, it will reset the min to 120mhz while screen is off and restore it upon screen awakening, and still scale accordingly to conservatives laws. So it spends most of its time at lower frequencies. The goal of this is to get the best battery life with decent performance. It is a performance focused governor but also blends with some battery savings.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -627,6 +627,9 @@ extern struct cpufreq_governor cpufreq_gov_cultivation;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CYAN)
 extern struct cpufreq_governor cpufreq_gov_cyan;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cyan)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_dancedance)
 #endif
 
 /*********************************************************************
