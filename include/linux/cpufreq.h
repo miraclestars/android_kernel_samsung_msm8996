8beed5fbf9ac25f79fb91bed1af079bffbd0e46e
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-07-11 00:34:02 +0900

    cpufreq: SmartassH3 Governor
    Based on SmartassV2 with parameters tuned by H3ROS. It has been tuned to have more efficient frequency scaling (not ramping too high when not needed) which this could potentially result in better battery life.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -726,6 +726,9 @@ extern struct cpufreq_governor cpufreq_gov_scary;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
 extern struct cpufreq_governor cpufreq_gov_slim;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
+extern struct cpufreq_governor cpufreq_gov_smartassH3;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartassH3)
 #endif
 
 /*********************************************************************
