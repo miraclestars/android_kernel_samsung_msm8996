a5799e0de43563b15eb7fc064cf4b4d80c721fea
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2019-08-06 00:48:27 +0900

    cpufreq: DarknessV5 Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -633,6 +633,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESSV5)
+extern struct cpufreq_governor cpufreq_gov_darknessv5;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darknessv5)
 #endif
 
 /*********************************************************************
