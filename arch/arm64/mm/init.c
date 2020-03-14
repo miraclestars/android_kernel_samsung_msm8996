0636e7324f96fa4a39b12ba7a45df230160e2b3f
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2020-03-15 01:30:58 +0900

    arm64: check for upper PAGE_SHIFT bits in pfn_valid()
    pfn_valid() returns a false positive when the lower (64 - PAGE_SHIFT)
    bits match a valid pfn but some of the upper bits are set.  This caused
    a kernel panic in kpageflags_read() when a userspace utility parsed
    /proc/*/pagemap, neglected to discard the upper flag bits, and tried to
    lseek()+read() from the corresponding offset in /proc/kpageflags.
    
    A valid pfn will never have the upper PAGE_SHIFT bits set, so simply
    check for this before passing the pfn to memblock_is_memory().
    
    Change-Id: Ief5d8cd4dd93cbecd545a634a8d5885865cb5970
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -119,11 +119,7 @@ static void __init zone_sizes_init(unsigned long min, unsigned long max)
 
 int pfn_valid(unsigned long pfn)
 {
-	phys_addr_t addr = pfn << PAGE_SHIFT;
-
-	if ((addr >> PAGE_SHIFT) != pfn)
-		return 0;
-	return memblock_is_memory(addr);
+	return (pfn & PFN_MASK) == pfn && memblock_is_memory(pfn << PAGE_SHIFT);
 }
 EXPORT_SYMBOL(pfn_valid);
 #endif
